7f4c9d4fe252fbd5cd53f9273f74c6cdb585740a
Xanthalas <xanthalas@live.co.uk>
2017-04-05 15:10:43 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SpotifyNowPlaying
 Get the currently playing track name from Spotify client and write to the command line
+
+
+This is a very simple command line client which will connect to the local Spotify client, retrieve the name and artist of the currently playing track and write it to the console.
+
+It was written to be used with [Limebar](https://github.com/xanthalas/Limebar).
+
+### Note
+
+When Spotify is running in offline mode then the Spotify API call will not be able to retrieve information. Switching Spotify to online, running SpotifyNowPlaying once, and then switching it back to offline seems to allow subsequent connections to work fine.
+
+It will also be unable to retrieve track data if Spotify is configured to use a Proxy server which it can't access.
---
+++ SpotifyNowPlaying/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using SpotifyAPI.Local;
+using SpotifyAPI.Local.Models;
+
+namespace SpotifyNowPlaying
+{
+    class Program
+    {
+
+        private static SpotifyLocalAPI _spotify;
+        static void Main(string[] args)
+        {
+
+            _spotify = new SpotifyLocalAPI();
+            bool connected = connect();
+
+            if (!connected)
+            {
+                return;
+            }
+
+            StatusResponse status = _spotify.GetStatus();
+            if (status == null || status.Track == null || status.Track.TrackResource == null || status.Track.ArtistResource == null)
+            {
+                Console.WriteLine("Cannot retrieve information from Spotify client");
+                return;
+            }
+            else
+            { 
+                var playing = (status.Playing) ? "" : " (paused)";
+
+                Console.WriteLine($"Now playing: {status.Track.TrackResource.Name} by {status.Track.ArtistResource.Name}{playing}");
+            }
+
+        }
+
+        private static bool connect()
+        {
+            if (!SpotifyLocalAPI.IsSpotifyRunning())
+            {
+                Console.WriteLine(@"Spotify isn't running!");
+                return false;
+            }
+            if (!SpotifyLocalAPI.IsSpotifyWebHelperRunning())
+            {
+                Console.WriteLine(@"SpotifyWebHelper isn't running!");
+                return false;
+            }
+
+            bool successful = _spotify.Connect();
+            if (!successful)
+            { 
+                Console.WriteLine(@"Couldn't connect to the spotify client.");
+            }
+
+            return successful;
+        }
+    }
+}
